2c5ae0fd4899a744c6f1ce17b0355bc3ad12cf2b
Kateryna Kovalchuk <120138618+Kateryna-Kovalchuk@users.noreply.github.com>
2025-03-21 17:34:23 +0100

    Update create_database.sql

--- scripts/create_database.sql
+++ scripts/create_database.sql
@@ -1,7 +1,9 @@
 -- 🔹 Перевірка, чи існує БД
 SELECT 1 FROM pg_database WHERE datname = 'DataWareHouse';
+
 --  Якщо нема, створюємо
 CREATE DATABASE "DataWarehouse";
+
 -- Перевірка та створення схем
 CREATE SCHEMA IF NOT EXISTS bronze;
 CREATE SCHEMA IF NOT EXISTS silver;
